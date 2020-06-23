861d20a31f99966a26c01d22e5360b7eea11db20
Raman Thatikunta <raman_k_t@hotmail.com>
2020-06-23 06:51:09 -0400

    First commit; Accessed departments in view using database-first approach

---
+++ TestDbFirst01MVC/Controllers/DepartmentsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestDbFirst01MVC.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        // GET: Departments
+        public ActionResult Index()
+        {
+            var context = new TestDbFirst01MVC.DAL.AdventureWorks2017Entities2();
+            var departments = context.Departments;
+
+            return View(departments);
+        }
+    }
+}
---
+++ TestDbFirst01MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestDbFirst01MVC.MvcApplication" Language="C#" %>
